65bcc32911911d575c9edcca4028eac9e40e6e78
donggoolosori <kdj6551@naver.com>
2021-06-14 18:46:04 +0900

    Refactor

--- Level3/13.순위/1.cpp
+++ Level3/13.순위/1.cpp
@@ -4,27 +4,20 @@
 
 using namespace std;
 
-int graph[101][101];
+bool graph[101][101];
 
 int solution(int n, vector<vector<int>> results) {
   int answer = 0;
 
-  for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= n; j++) {
-      if (i == j) continue;
-      graph[i][j] = 1e9;
-    }
-  }
-
   for (const auto &result : results) {
     int winner = result[0], loser = result[1];
-    graph[winner][loser] = 1;
+    graph[winner][loser] = true;
   }
 
   for (int k = 1; k <= n; k++) {
     for (int i = 1; i <= n; i++) {
       for (int j = 1; j <= n; j++) {
-        graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
+        if (graph[i][k] && graph[k][j]) graph[i][j] = true;
       }
     }
   }
@@ -32,9 +25,9 @@ int solution(int n, vector<vector<int>> results) {
   for (int i = 1; i <= n; i++) {
     int cnt = 0;
     for (int j = 1; j <= n; j++) {
-      if (graph[i][j] != 1e9 || graph[j][i] != 1e9) cnt++;
+      if (graph[i][j] || graph[j][i]) cnt++;
     }
-    if (cnt == n) answer++;
+    if (cnt == n - 1) answer++;
   }
 
   return answer;
