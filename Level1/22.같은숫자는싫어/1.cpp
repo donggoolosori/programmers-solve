56dab630a141baddfd77c88a207c4b3118fb10cb
donggoolosori <kdj6551@naver.com>
2021-06-23 11:58:50 +0900

    solve:use unique method

---
+++ Level1/22.같은숫자는싫어/1.cpp
@@ -0,0 +1,11 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+  arr.erase(unique(arr.begin(), arr.end()), arr.end());
+
+  return arr;
+}
