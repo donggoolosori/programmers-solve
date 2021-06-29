23b320e8a8257db6471a11d6a7a2647570bf6e27
donggoolosori <kdj6551@naver.com>
2021-06-29 16:14:46 +0900

    refactor: don't need compare function

--- Level3/28.단속카메라/1.cpp
+++ Level3/28.단속카메라/1.cpp
@@ -1,14 +1,8 @@
 #include <algorithm>
-#include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
-
-bool compare(vector<int> a, vector<int> b) {
-  if (a[0] == b[0]) return a[1] > b[1];
-  return a[0] < b[0];
-}
 
 int solution(vector<vector<int>> routes) {
   int answer = 0;
