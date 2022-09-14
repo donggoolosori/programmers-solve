62ff4b9fbfaebe865fa351b658658d4dafbcde99
donggoolosori <kdj6551@naver.com>
2022-09-15 00:31:17 +0900

    solve(lv2): 하노이의 탑

---
+++ Level2/하노이의 탑/1.js
@@ -0,0 +1,17 @@
+function solution(n) {
+  const ret = [];
+
+  function hanoi(num, fr, to, other) {
+    if (num === 0) {
+      return;
+    }
+
+    hanoi(num - 1, fr, other, to);
+    ret.push([fr, to]);
+    hanoi(num - 1, other, to, fr);
+  }
+
+  hanoi(n, 1, 3, 2);
+
+  return ret;
+}
