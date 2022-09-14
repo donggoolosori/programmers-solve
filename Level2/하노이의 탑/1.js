b7f75579bb0819e852048c665fb669706600bff5
donggoolosori <kdj6551@naver.com>
2022-09-15 00:40:15 +0900

    tabwidth 변경

--- Level2/하노이의 탑/1.js
+++ Level2/하노이의 탑/1.js
@@ -1,17 +1,17 @@
 function solution(n) {
-  const ret = [];
+    const ret = [];
 
-  function hanoi(num, fr, to, other) {
-    if (num === 0) {
-      return;
+    function hanoi(num, fr, to, other) {
+        if (num === 0) {
+            return;
+        }
+
+        hanoi(num - 1, fr, other, to);
+        ret.push([fr, to]);
+        hanoi(num - 1, other, to, fr);
     }
 
-    hanoi(num - 1, fr, other, to);
-    ret.push([fr, to]);
-    hanoi(num - 1, other, to, fr);
-  }
+    hanoi(n, 1, 3, 2);
 
-  hanoi(n, 1, 3, 2);
-
-  return ret;
+    return ret;
 }
